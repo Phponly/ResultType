3ae39726f78a2eab374aa7fabea446997c6a0578
Fulgur <fulgurgithub@gmail.com>
2024-12-09 17:05:09 +0100

    Update Success.php

--- Success.php
+++ Success.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace GrahamCampbell\ResultType;
+namespace Phponly\ResultType;
 
 use Phponly\Onlyoption\None;
 use Phponly\Onlyoption\Some;
